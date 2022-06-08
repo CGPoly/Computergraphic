9bfcf2be301b01d6723b58764d727d9a9835f8e5
rihi <19492038+rihi@users.noreply.github.com>
2022-06-08 23:05:41 +0200

    Some minor syntax stuff in common.cpp

--- src/library/common.cpp
+++ src/library/common.cpp
@@ -1,31 +1,31 @@
 #include "common.hpp"
 
-void APIENTRY glDebugOutput(GLenum source,
-                            GLenum type,
-                            unsigned int id,
-                            GLenum severity,
-                            GLsizei length,
-                            const char *message,
-                            const void *userParam)
-{
+void APIENTRY glDebugOutput(
+		GLenum source,
+		GLenum type,
+		unsigned int id,
+		GLenum severity,
+		GLsizei length,
+		const char *message,
+		const void *userParam
+) {
 	// ignore non-significant error/warning codes
 	if(id == 131169 || id == 131185 || id == 131218 || id == 131204) return;
 
 	std::cout << "---------------" << std::endl;
 	std::cout << "Debug message (" << id << "): " <<  message << std::endl;
 
-	switch (source)
-	{
+	switch (source) {
 		case GL_DEBUG_SOURCE_API:             std::cout << "Source: API"; break;
 		case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   std::cout << "Source: Window System"; break;
 		case GL_DEBUG_SOURCE_SHADER_COMPILER: std::cout << "Source: Shader Compiler"; break;
 		case GL_DEBUG_SOURCE_THIRD_PARTY:     std::cout << "Source: Third Party"; break;
 		case GL_DEBUG_SOURCE_APPLICATION:     std::cout << "Source: Application"; break;
 		case GL_DEBUG_SOURCE_OTHER:           std::cout << "Source: Other"; break;
-	} std::cout << std::endl;
+	}
+	std::cout << std::endl;
 
-	switch (type)
-	{
+	switch (type) {
 		case GL_DEBUG_TYPE_ERROR:               std::cout << "Type: Error"; break;
 		case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: std::cout << "Type: Deprecated Behaviour"; break;
 		case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  std::cout << "Type: Undefined Behaviour"; break;
@@ -35,15 +35,16 @@ void APIENTRY glDebugOutput(GLenum source,
 		case GL_DEBUG_TYPE_PUSH_GROUP:          std::cout << "Type: Push Group"; break;
 		case GL_DEBUG_TYPE_POP_GROUP:           std::cout << "Type: Pop Group"; break;
 		case GL_DEBUG_TYPE_OTHER:               std::cout << "Type: Other"; break;
-	} std::cout << std::endl;
+	}
+	std::cout << std::endl;
 
-	switch (severity)
-	{
+	switch (severity) {
 		case GL_DEBUG_SEVERITY_HIGH:         std::cout << "Severity: high"; break;
 		case GL_DEBUG_SEVERITY_MEDIUM:       std::cout << "Severity: medium"; break;
 		case GL_DEBUG_SEVERITY_LOW:          std::cout << "Severity: low"; break;
 		case GL_DEBUG_SEVERITY_NOTIFICATION: std::cout << "Severity: notification"; break;
-	} std::cout << std::endl;
+	}
+	std::cout << std::endl;
 	std::cout << std::endl;
 }
 
@@ -53,7 +54,7 @@ initOpenGL(int width, int height, const char* title) {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true);
+	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
 #endif
