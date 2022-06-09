bdc47d3d9a9c9c122126b661428be83888b4f808
rihi <19492038+rihi@users.noreply.github.com>
2022-06-09 16:44:03 +0200

    Remove now unused helper file

--- src/library/helper.cpp
+++
@@ -1,8 +0,0 @@
-#include "helper.hpp"
-
-time_t get_filetime(const std::string filename) {
-    struct stat result;
-    if (stat(filename.c_str(), &result) == 0) {
-        return result.st_mtime;
-    }
-}
